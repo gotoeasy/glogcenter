3464249f76223ae7075829070fea38ab55be0614
gotoeasy <gotoeasy@163.com>
2022-06-27 14:29:38 +0800

    配置名统一前缀“GLC_”

--- glc/ldb/conf/config.go
+++ glc/ldb/conf/config.go
@@ -16,31 +16,45 @@ var storeRoot string
 var storeChanLength int
 var maxIdleTime int
 var storeNameAutoAddDate bool
+var serverPort int
+var contextPath string
 
 func init() {
 	// 读取环境变量初始化配置，各配置都有默认值
-	storeRoot = Getenv("STORE_ROOT", "/glogcenter")                     // 存储根目录
-	storeChanLength = GetenvInt("STORE_CHAN_LENGTH", 64)                // 存储通道长度
-	maxIdleTime = GetenvInt("MAX_IDLE_TIME", 180)                       // 最大闲置时间（秒）,超过闲置时间将自动关闭，0时表示不关闭
-	storeNameAutoAddDate = GetenvBool("STORE_NAME_AUTO_ADD_DATE", true) // 存储名是否自动添加日期（日志量大通常按日单位区分存储），默认true
+	storeRoot = Getenv("GLC_STORE_ROOT", "/glogcenter")                     // 存储根目录
+	storeChanLength = GetenvInt("GLC_STORE_CHAN_LENGTH", 64)                // 存储通道长度
+	maxIdleTime = GetenvInt("GLC_MAX_IDLE_TIME", 180)                       // 最大闲置时间（秒）,超过闲置时间将自动关闭，0时表示不关闭
+	storeNameAutoAddDate = GetenvBool("GLC_STORE_NAME_AUTO_ADD_DATE", true) // 存储名是否自动添加日期（日志量大通常按日单位区分存储），默认true
+	serverPort = GetenvInt("GLC_SERVER_PORT", 8080)                         // web服务端口
+	contextPath = Getenv("GLC_CONTEXT_PATH", "/glc")                        // web服务contextPath
 }
 
-// 取配置：存储根目录，可通过环境变量“STORE_ROOT”设定，默认值“/glogcenter”
+// 取配置： web服务端口，可通过环境变量“GLC_CONTEXT_PATH”设定，默认值“8080”
+func GetContextPath() string {
+	return contextPath
+}
+
+// 取配置： web服务端口，可通过环境变量“GLC_SERVER_PORT”设定，默认值“8080”
+func GetServerPort() int {
+	return serverPort
+}
+
+// 取配置：存储根目录，可通过环境变量“GLC_STORE_ROOT”设定，默认值“/glogcenter”
 func GetStorageRoot() string {
 	return storeRoot
 }
 
-// 取配置：存储通道长度，可通过环境变量“STORE_CHAN_LENGTH”设定，默认值“64”
+// 取配置：存储通道长度，可通过环境变量“GLC_STORE_CHAN_LENGTH”设定，默认值“64”
 func GetStoreChanLength() int {
 	return storeChanLength
 }
 
-// 取配置：最大闲置时间（秒），可通过环境变量“MAX_IDLE_TIME”设定，默认值“180”，超过闲置时间将自动关闭存储器，0时表示不关闭
+// 取配置：最大闲置时间（秒），可通过环境变量“GLC_MAX_IDLE_TIME”设定，默认值“180”，超过闲置时间将自动关闭存储器，0时表示不关闭
 func GetMaxIdleTime() int {
 	return maxIdleTime
 }
 
-// 取配置：存储名是否自动添加日期（日志量大通常按日单位区分存储），可通过环境变量“STORE_NAME_AUTO_ADD_DATE”设定，默认值“true”
+// 取配置：存储名是否自动添加日期（日志量大通常按日单位区分存储），可通过环境变量“GLC_STORE_NAME_AUTO_ADD_DATE”设定，默认值“true”
 func IsStoreNameAutoAddDate() bool {
 	return storeNameAutoAddDate
 }
