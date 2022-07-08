95c66e1b8e980fe9f537baed1b218c5bb1691ceb
gotoeasy <gotoeasy@163.com>
2022-07-08 11:27:12 +0800

    友好提示

--- glc/www/web/src/views/storages.vue
+++ glc/www/web/src/views/storages.vue
@@ -81,7 +81,7 @@ export default {
   methods: {
     remove(row) {
 
-      this.$confirm('确定删除吗？', '提示', {
+      this.$confirm('确定删除 ' + row.name + ' 吗？', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning',
