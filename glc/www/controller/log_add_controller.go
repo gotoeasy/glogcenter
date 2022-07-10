01379e3df0fb4ae41219e7ce55e011dc81817615
gotoeasy <gotoeasy@163.com>
2022-07-10 20:18:47 +0800

    删除旧版不推荐的接口

--- glc/www/controller/log_add_controller.go
+++ glc/www/controller/log_add_controller.go
@@ -8,26 +8,6 @@ import (
 	"log"
 )
 
-// 添加日志（表单提交方式）
-func LogAddController(req *gweb.HttpRequest) *gweb.HttpResult {
-
-	// 开启API秘钥校验时才检查
-	if conf.IsEnableSecurityKey() {
-		auth := req.GetHeader(conf.GetHeaderSecurityKey())
-		if auth != conf.GetSecurityKey() {
-			return gweb.Error(403, "未经授权的访问，拒绝服务")
-		}
-	}
-
-	text := req.GetFormParameter("text")
-	date := req.GetFormParameter("date")
-	system := req.GetFormParameter("system")
-
-	engine := ldb.NewDefaultEngine()
-	engine.AddTextLog(date, text, system)
-	return gweb.Ok()
-}
-
 // 添加日志（JSON提交方式）
 func JsonLogAddController(req *gweb.HttpRequest) *gweb.HttpResult {
 
