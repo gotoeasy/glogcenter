ada476b8c9bef2ce38df3b42b2e2f74e59e686f0
gotoeasy <gotoeasy@163.com>
2022-11-29 15:37:29 +0800

    增加CPU配置

--- glc/main.go
+++ glc/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "glc/onstart"
+import (
+	"glc/conf"
+	"glc/onstart"
+	"runtime"
+)
 
 func main() {
+	runtime.GOMAXPROCS(conf.GetGoMaxProcess()) // 使用最大CPU数量
 	onstart.Run()
 }
