0e40f0c98a082255cb57a10515629012d9a452c8
gotoeasy <gotoeasy@163.com>
2023-08-15 12:37:19 +0800

    跨域配置

--- glc/www/filter/filter.go
+++ glc/www/filter/filter.go
@@ -1,6 +1,7 @@
 package filter
 
 import (
+	"glc/conf"
 	"glc/gweb"
 	"net/http"
 )
@@ -8,15 +9,18 @@ import (
 // 允许跨域
 func CrossFilter(req *gweb.HttpRequest) *gweb.HttpResult {
 
-	req.SetHeader("Access-Control-Allow-Origin", "*")
-	req.SetHeader("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
-	req.SetHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	req.SetHeader("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-	req.SetHeader("Access-Control-Allow-Credentials", "true")
+	if conf.IsEnableCross() {
+		req.SetHeader("Access-Control-Allow-Origin", "*")
+		req.SetHeader("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		req.SetHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		req.SetHeader("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		req.SetHeader("Access-Control-Allow-Credentials", "true")
 
-	//放行所有OPTIONS方法
-	if req.GetMethod() == "OPTIONS" {
-		req.AbortWithStatus(http.StatusNoContent)
+		//放行所有OPTIONS方法
+		if req.GetMethod() == "OPTIONS" {
+			req.AbortWithStatus(http.StatusOK)
+		}
 	}
+
 	return nil
 }
