4429f95f2045af8c1e88b5474fadc6cf3e9408a3
gotoeasy <gotoeasy@163.com>
2024-09-06 22:12:18 +0800

    v0.17.0

--- glc/main.go
+++ glc/main.go
@@ -10,9 +10,8 @@ import (
 
 func main() {
 	cmn.SetGlcClient(cmn.NewGlcClient(&cmn.GlcOptions{
-		Enable:           false,                                           // 禁止直接发送到GLC
-		EnableConsoleLog: cmn.GetEnvBool("GLC_ENABLE_CONSOLE_LOG", false), // 关闭控制台日志输出
-		LogLevel:         cmn.GetEnvStr("GLC_LOG_LEVEL", "INFO"),          // 控制台INFO日志级别输出
+		EnableConsoleLog: cmn.GetEnvStr("GLC_ENABLE_CONSOLE_LOG", "false"), // 关闭控制台日志输出
+		LogLevel:         cmn.GetEnvStr("GLC_LOG_LEVEL", "INFO"),           // 控制台INFO日志级别输出
 	}))
 
 	runtime.GOMAXPROCS(conf.GetGoMaxProcess()) // 使用最大CPU数量
